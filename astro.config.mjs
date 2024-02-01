75072d1fb4d44fbd3422c5b64a28a56f687ff264
Xavier Fenton <xavierfenton99@gmail.com>
2024-02-02 12:29:13 +1300

    add vercel dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,20 @@
-import { sanityIntegration } from '@sanity/astro'
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import react from '@astrojs/react'
+import { sanityIntegration } from '@sanity/astro';
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
-  integrations: [
-    tailwind(),
-    sanityIntegration({
-      projectId: '07dp7kpt',
-      dataset: 'production',
-      apiVersion: '2023-02-08',
-      useCdn: false,
-      // Access the Studio on your.url/admin
-      studioBasePath: '/admin',
-    }),
-    react(),
-  ],
-})
+  integrations: [tailwind(), sanityIntegration({
+    projectId: '07dp7kpt',
+    dataset: 'production',
+    apiVersion: '2023-02-08',
+    useCdn: false,
+    // Access the Studio on your.url/admin
+    studioBasePath: '/admin'
+  }), react()],
+  adapter: vercel()
+});
